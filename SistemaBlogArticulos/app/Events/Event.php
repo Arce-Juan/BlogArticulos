26675e58a404772c19681449f11b478ea4a4050a
Tu Nombre <you@example.com>
2020-02-20 22:42:43 -0300

    se elimino carpeta

--- SistemaBlogArticulos/app/Events/Event.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace blogArticulo\Events;
-
-abstract class Event
-{
-    //
-}
--- SistemaBlogArticulos/resources/views/blogArticulo/principal/buscar.blade.php
+++
@@ -1,8 +0,0 @@
-@extends ('layouts.admin')
-@section ('contenido')
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <h5>Comentarios</h5>
-    </div>
-</div>
-@endsection
