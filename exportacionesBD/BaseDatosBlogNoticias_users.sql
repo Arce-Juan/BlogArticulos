ba802d43eb09a00839c2c871bfe12bb2fa88aaef
Tu Nombre <you@example.com>
2020-02-24 01:51:46 -0300

    se actualizo base de datos

--- exportacionesBD/BaseDatosBlogNoticias_users.sql
+++ exportacionesBD/BaseDatosBlogNoticias_users.sql
@@ -48,7 +48,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (6,'mili','$2y$10$GVcVQIUJiM99faVA1J9IX.jW3go.7mqbbmzMNI8yU4mApfF/1ShEK','usuario4.jpg',1,'vazques','mili','soyMili@mail.com',4,'O9kb8swFjEaeqmthKYyFDg50SzbGptXoHLoW0ZMUVglSJUwWtNUu4l73KT7i',NULL,NULL),(7,'Invitado','$2y$10$tsvPNoC6FXZCQV80SHDVzez7G3hO.Nd1ve08IUFT1YDr2hjeDqxO6','default.png',1,'invitado','invitado','invitado@invitado.com',6,'WJRjB0gTGHdmwg5QUJZWn5e7wThGSMDpmuBbcvXRsmYyyNbpA7QRpgbTqR19',NULL,NULL),(16,'admin','$2y$10$MNIExwchYZRQZlpZ/AZZ0eL2j2h45RNQte/XjuDUfn/W2djGvFTai','default.png',1,'admin','admin','admin@admin.com',5,'vbK2Rjlrkxblm1ptINgFLLiQhUrF1sgAy6YjbmOc5vwSJAV9V0blxUCJ9aNq',NULL,NULL),(18,'chanchi','$2y$10$WKYmz/SBImSK38U0dRXNK.MAp4NczA69R6ZQbg4e1ghbpKuJLtoG.','usuario6.jpg',1,'arce','chanchon','chanchi@chanchi.com',7,'hcATXUJeicrgUTdYQgi9quvETnqqSCDA4nmtcRuDl8ZGOG37Kpw5er1cRStd',NULL,NULL),(19,'arcej','$2y$10$4InxK57FM5lWzAQyMZ1nb.dMpqtsl2uB8AWYE1OBegKhYeaniGmzW','default.png',1,'arce','juan','juan@juan.com',4,'nRwygfxVX26mI0K6HtBs3xlyFqw8t6dVAZrsrUdXZSWbrzDqBXgkJdOvF9Mh',NULL,NULL);
+INSERT INTO `users` VALUES (6,'mili','$2y$10$GVcVQIUJiM99faVA1J9IX.jW3go.7mqbbmzMNI8yU4mApfF/1ShEK','usuario4.jpg',1,'vazques','mili','soyMili@mail.com',4,'O9kb8swFjEaeqmthKYyFDg50SzbGptXoHLoW0ZMUVglSJUwWtNUu4l73KT7i',NULL,NULL),(7,'Invitado','$2y$10$tsvPNoC6FXZCQV80SHDVzez7G3hO.Nd1ve08IUFT1YDr2hjeDqxO6','default.png',1,'invitado','invitado','invitado@invitado.com',6,'pEKcmqj9nNgBpi51BoUBrwi3c6NYNN1jux9wJIaeR10Jz8HQTT7T4rYlwZpw',NULL,NULL),(16,'admin','$2y$10$JKVMW3CmCy06hymbFrIIieWRjw7fopROr6nW/.ZPQUOxFLnID4fVG','admin-icon.png',1,'admin','admin','admin@admin.com',5,'ei9juOKCQAkqTiUMgWj3LcUcHdRpmCLv6AzVU3jJXsncB1myu7bjHviFoZq0',NULL,NULL),(18,'chanchi','$2y$10$WKYmz/SBImSK38U0dRXNK.MAp4NczA69R6ZQbg4e1ghbpKuJLtoG.','usuario6.jpg',1,'arce','chanchon','chanchi@chanchi.com',7,'8OVLMYMfViud4Ky0Gb5i75E5f7Q8VNbD4lKZBc3pz0eae7XiMVMMY2bEfkKk',NULL,NULL),(19,'arcej','$2y$10$4InxK57FM5lWzAQyMZ1nb.dMpqtsl2uB8AWYE1OBegKhYeaniGmzW','usuario1.jpg',1,'arce','juan','juan@juan.com',4,'AhGjxF4rz4eBS36xt1szqXrpIBynmOKqNNR3f6otZvg07L6r4awNiup4fwlX',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -61,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-23  0:59:37
+-- Dump completed on 2020-02-24  1:50:56
